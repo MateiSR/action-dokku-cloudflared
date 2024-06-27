df8b61fd5d9c1d55141b7c63c8ba9bfbcef774f4
MateiSR <MateiSR@proton.me>
2024-06-27 17:28:13 +0300

    fix(ssh): connection fix

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,7 +8,7 @@ script_dir=$(dirname $0)
 app=$(echo $DOKKU_REPO | sed -e 's/.*\///')
 echo "Parsed app name: $app"
 
-echo "Pushing to Dokku Host"
+echo "Pushing to Dokku Host: $ssh_host"
 command="git push $ssh_user@$ssh_host:$app refs/heads/$DEPLOY_BRANCH --force"
 echo "git command: $command"
 
